ef380500c91d870fdd35614762f5bfd86155382a
bl4ko <gasperoblak@bl4ko.com>
2026-08-09 18:31:43 +0200

    fix(terminal): settle tmux shell cursor after newlines

--- Packages/Core/Package.swift
+++ Packages/Core/Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-crypto.git", from: "3.0.0"),
         .package(
             url: "https://github.com/bl4ko/SwiftTerm.git",
-            revision: "ac903b6dfb6e90a3cee553d0fcef90f5006e1c75"),
+            revision: "a175e84ac69b36630f634a4dc50d214a75e29f4c"),
         .package(
             url: "https://github.com/bl4ko/royalvnc.git",
             revision: "7cd41cff27e175c12b552dce2812ff78b35eb541"),
